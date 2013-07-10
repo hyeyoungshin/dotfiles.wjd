4c2b82d1a486b9fc879bcdefb58854a46ebadddd
William DeMeo <williamdemeo@gmail.com>
2013-07-10 16:51:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+To use the config files in the dotfiles.wjd repo, 
+just download the setup.sh file and run it from the 
+command line like this:
+
+  chmod a+x setup.sh
+  ./setup.sh
+
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make sure git is installed:
+sudo apt-get install -y git-core
+
+# Backup any previous versions:
+cd $HOME
+if [ -d ./dotfiles.wjd/ ]; then
+    mv dotfiles dotfiles.wjd.old
+fi
+
+# Get the dotfiles.wjd repository:
+git clone https://github.com/williamdemeo/dotfiles.wjd.git
+
+# Create the required links:
+ln -sb --suffix='.orig' dotfiles.wjd/bashrc.wjd ./.bashrc
+ln -sb --suffix='.orig' dotfiles.wjd/bashrc_custom.wjd ./.bashrc_custom
+ln -sb --suffix='.orig' dotfiles.wjd/bash_profile.wjd ./.bash_profile
+ln -sb --suffix='.orig' dotfiles.wjd/bash_aliases.wjd ./.bash_aliases
+ln -sb --suffix='.orig' dotfiles.wjd/profile.wjd ./.profile
+
