1da5fe54eb75ccba75d71f41d359113776d1262d
William DeMeo <williamdemeo@gmail.com>
2013-07-23 14:47:12 -0400

    change to normal backspace behavior

--- emacs.d.wjd/init.el
+++ emacs.d.wjd/init.el
@@ -18,7 +18,7 @@
 (require 'compile)
 ;(ido-mode t)
 (menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
+;(normal-erase-is-backspace-mode 1)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -64,7 +64,7 @@
 (global-set-key "\C-h" 'backward-delete-char)
 (global-set-key "\M-d" 'delete-word)
 (global-set-key "\M-h" 'backward-delete-word)
-(global-set-key "\M-u" 'zap-to-char)
+;(global-set-key "\M-u" 'zap-to-char)
 
 ;; ---------------------------
 ;; -- JS Mode configuration --
