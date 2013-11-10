0295107bdf7778b4f9425d9cd2d863ba51bee026
William DeMeo <williamdemeo@gmail.com>
2013-11-10 02:19:58 -0500

    added Scala stuff

--- emacs.d.wjd/custom_wjd_init.el
+++ emacs.d.wjd/custom_wjd_init.el
@@ -33,6 +33,15 @@
 ;; \M package-install [Enter] magit
 (define-key global-map "\M-gm" 'magit-status)
 
+;;
+;; For Scala
+;;
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'scala-mode2)
+  (package-refresh-contents) (package-install 'scala-mode2))
 
 
 ;;; uncomment this line to disable loading of "default.el" at startup
